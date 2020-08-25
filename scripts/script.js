f0701ce9b9423ca0f7e5f66470f351fa01ea5026
troylochner <troy.lochner@gmail.com>
2020-08-25 01:47:20 -0400

    working question validator

--- scripts/script.js
+++ scripts/script.js
@@ -1,59 +1,97 @@
 //declare my variables for page elements
 var questionBox = document.querySelector("#questionBox");
 var answerBox = document.querySelector("#answerBox");
+var resultBox = document.querySelector("#resultBox");
 var timerDisplay = document.querySelector("#timerDisplay");
 var testButton = document.querySelector("#tester");
+var prevButton = document.querySelector("#prev");
+var nextButton = document.querySelector("#next");
 
 //minified the json string for ease of reading in larger code. 
-var questionBook = {"questions":[{"question":"Pick a letter - the answer is a","answer":"1","choices":[{"heading":"a","answer":"The letter A"},{"heading":"b","answer":"The letter B"},{"heading":"c","answer":"The letter C"},{"heading":"d","answer":"The letter D"}]},{"question":"Pick a letter - the answer is b","answer":"2","choices":[{"heading":"a","answer":"The letter A"},{"heading":"b","answer":"The letter B"},{"heading":"c","answer":"The letter C"},{"heading":"d","answer":"The letter D"}]},{"question":"Pick a letter - the answer is c","answer":"3","choices":[{"heading":"a","answer":"The letter A"},{"heading":"b","answer":"The letter B"},{"heading":"c","answer":"The letter C"},{"heading":"d","answer":"The letter D"}]},{"question":"Pick a letter - the answer is d","answer":"4","choices":[{"heading":"a","answer":"The letter A"},{"heading":"b","answer":"The letter B"},{"heading":"c","answer":"The letter C"},{"heading":"d","answer":"The letter D"}]}]};
+var questionBook = {"questions":[{"question":"Pick a letter - the answer is a","answer":0,"choices":[{"heading":"a","answer":"The letter A"},{"heading":"b","answer":"The letter B"},{"heading":"c","answer":"The letter C"},{"heading":"d","answer":"The letter D"}]},{"question":"Pick a letter - the answer is b","answer":1,"choices":[{"heading":"a","answer":"The letter A"},{"heading":"b","answer":"The letter B"},{"heading":"c","answer":"The letter C"},{"heading":"d","answer":"The letter D"}]},{"question":"Pick a letter - the answer is c","answer":2,"choices":[{"heading":"a","answer":"The letter A"},{"heading":"b","answer":"The letter B"},{"heading":"c","answer":"The letter C"},{"heading":"d","answer":"The letter D"}]},{"question":"Pick a letter - the answer is d","answer":3,"choices":[{"heading":"a","answer":"The letter A"},{"heading":"b","answer":"The letter B"},{"heading":"c","answer":"The letter C"},{"heading":"d","answer":"The letter D"}]}]};
 
 
 //declare the functions needed to run this application. 
-function renderQuestionPrompt() {
+function loadQuestion(indexQuestion) {
     // Clear question box
     questionBox.innerHTML = "";
-    questionBox.innerText=questionBook.questions[0].question
+    questionBox.innerText=questionBook.questions[indexQuestion].question
     
     //CREATE AN OBJECT OF THE QUESTION + ANSWERS
-    var questionObject = questionBook.questions[0] // move to global var after QC
+    var questionObject = questionBook.questions[indexQuestion] // move to global var after QC
     console.log("questionObject :" + questionObject);
-    console.log("length :" + questionObject.choices.length )
+    console.log("The right answer : "+  questionObject.answer);
+    console.log("length :" + questionObject.choices.length );
     
-// Render a new line for each question
+// Render a new line for each choice
     for (var i = 0; i < questionObject.choices.length; i++) {
-      //console.log(i + "|" + questionObject.choices.length );
-      //var questionObject = questions[0].choices[0].header[i] + ". |" + questions[0].choices[0].answer[i] ; //move to global var after QC
-
-      var theQuestion = JSON.stringify(questionObject.choices[i])  ; 
-      console.log(theQuestion) ;
+      var theQuestion = JSON.stringify(questionObject.choices[i]) ; 
 
       //CREATE AND APPEND OUR CHOICE
-      var li = document.createElement("li"); // or child element
-      li.textContent = theQuestion;
-      console.log(li.textContent)
-      li.setAttribute("data-index", i);
-      
+      var myP = document.createElement("p"); // or child element
+      myP.textContent = questionObject.choices[i].heading + " | " + questionObject.choices[i].answer ;      ;
+      myP.setAttribute("data-index", i);
+      myP.setAttribute("data-parent-id",indexQuestion)
+      myP.setAttribute("id","theChoices")
+      console.log(theQuestion) ;     
 
       //CREATE AND APPEND THE BUTTON
-  
       var button = document.createElement("button");
-      button.textContent = "I choose this !";  
-      li.appendChild(button);
+      button.textContent = "|SELECT|";
+      button.setAttribute("style", "background-color: red;");
+      button.setAttribute("data-parent-id",indexQuestion)
+      button.setAttribute("data-index", i);
+      myP.appendChild(button);
+      
+      //APPEND EVERYTHIN TO THE ANSWER BOX
+      answerBox.appendChild(myP);
 
-//add button 
-      answerBox.appendChild(li);
     }
   }
-  
+
 
 /*function init(){};*/
-function checkAnswer (){};
+function checkAnswer (questionIndex,answerIndex){
+  var theQuestion = questionBook.questions[questionIndex]
+  var rightAnswer = theQuestion.answer
+
+  if (answerIndex == rightAnswer ){
+    confirm("You are smart.");
+    //ADD TO SCORE
+  } else {
+  confirm("You are kind of stupid.");
+  }
+  //ADD TO SCORE
+  //console.log("Checking question : " + questionIndex);
+  //console.log("You answered : " + answerIndex );
+  //console.log("YThe right answer is " + rightAnswer );
+
+
+
+
+
+};
 function updateScore(){};
 function saveHighScore(){};
 
 
 //ADDED EVENT LISTENERS
 tester.addEventListener("click", function(){
-    renderQuestionPrompt();
+    loadQuestion(3);
+})
+
+//LISTEN FOR ANSWER
+answerBox.addEventListener("click", function(event){
+  var element = event.target ;
+  if (element.matches("button")===true){
+    var qIndex = element.getAttribute("data-parent-id");
+    var aindex = element.getAttribute("data-index") ;
+    
+    //console.log("You choose " + aindex + " Q# " + qIndex);
+
+    checkAnswer(qIndex,aindex)
+
+    
+  }
 })
 
